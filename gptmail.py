1cf3207cca4bc14eedda06fe7cf720c68e8a37fe
pepeette <59664130+pepeette@users.noreply.github.com>
2023-11-03 10:28:49 +0700

    Update gptmail.py

--- gptmail.py
+++ gptmail.py
@@ -1,3 +1,30 @@
+# template = """
+#     Below is an email that may be poorly worded.
+#     Your goal is to:
+#     - Properly format the email
+#     - Convert the input text to a specified tone
+#     - Convert the input text to a specified dialect
+
+#     Here are some examples different Tones:
+#     - Formal: We went to Barcelona for the weekend. We have a lot of things to tell you.
+#     - Informal: Went to Barcelona for the weekend. Lots to tell you.  
+
+#     Here are some examples of words in different dialects:
+#     - American: French Fries, cotton candy, apartment, garbage, cookie, green thumb, parking lot, pants, windshield
+#     - British: chips, candyfloss, flag, rubbish, biscuit, green fingers, car park, trousers, windscreen
+
+#     Example Sentences from each dialect:
+#     - American: I headed straight for the produce section to grab some fresh vegetables, like bell peppers and zucchini. After that, I made my way to the meat department to pick up some chicken breasts.
+#     - British: Well, I popped down to the local shop just the other day to pick up a few bits and bobs. As I was perusing the aisles, I noticed that they were fresh out of biscuits, which was a bit of a disappointment, as I do love a good cuppa with a biscuit or two.
+
+#     Please start the email with a warm introduction. Add the introduction if you need to.
+    
+#     Below is the email, tone, and dialect:
+#     TONE: {tone}
+#     DIALECT: {dialect}
+#     EMAIL: {email}
+    
+# """
 import streamlit as st
 from langchain.prompts import PromptTemplate
 from langchain.llms import OpenAI
@@ -59,15 +86,21 @@ st.write("""
 
 col1, col2 = st.columns([10, 10])
 with col1:
-    sender = st.text_input(label="Sender's Name", key="sender_input")
+    sender = st.text_input(label="Name of the client", key="sender_input")
 with col2:
-    recipient = st.text_input(label="Recipient's Name", key="recipient_input")
+    typology = st.text_input(label="Type of client", key="typology_input")
 
 def get_text():
     input_text = st.text_area(label="Paste the email here",  placeholder="Your Email...", key="email_input")
     return input_text
 
 email_input = get_text()
+
+col3, col4 = st.columns([10, 10])
+with col3:
+    tone = st.text_input(label="Response tone", key="tone_input")
+with col4:
+    recipient = st.text_input(label="Your name", key="recipient_input")
 
 if len(email_input.split(" ")) > 700:
     st.write("Please enter a shorter email. The maximum length is 700 words.")
@@ -87,3 +120,4 @@ if st.button("Generate REPLY", type='secondary', help="Click to see an example o
         formatted_email = llm(prompt_with_email)
 
         st.write(formatted_email)
+
