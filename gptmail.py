c5c3129844b2800baf283e18d828e1d2c8704505
pepeette <59664130+pepeette@users.noreply.github.com>
2023-11-06 09:46:31 +0700

    Update gptmail.py

--- gptmail.py
+++ gptmail.py
@@ -3,19 +3,17 @@
 #     Your goal is to:
 #     - Properly format the email
 #     - Convert the input text to a specified tone
-#     - Convert the input text to a specified dialect
+#     - Convert the input text to address a specific typology
 
 #     Here are some examples different Tones:
 #     - Formal: We went to Barcelona for the weekend. We have a lot of things to tell you.
 #     - Informal: Went to Barcelona for the weekend. Lots to tell you.  
 
-#     Here are some examples of words in different dialects:
-#     - American: French Fries, cotton candy, apartment, garbage, cookie, green thumb, parking lot, pants, windshield
-#     - British: chips, candyfloss, flag, rubbish, biscuit, green fingers, car park, trousers, windscreen
-
-#     Example Sentences from each dialect:
-#     - American: I headed straight for the produce section to grab some fresh vegetables, like bell peppers and zucchini. After that, I made my way to the meat department to pick up some chicken breasts.
-#     - British: Well, I popped down to the local shop just the other day to pick up a few bits and bobs. As I was perusing the aisles, I noticed that they were fresh out of biscuits, which was a bit of a disappointment, as I do love a good cuppa with a biscuit or two.
+#     Here are some examples of words in different typology:
+#     - Arsey: He thinks he knows better and is inpolite, the best reponse to this typology is to name drop, 
+#    state that we serve the best clients, that we do and we know what we do, we are the expert, respond in expert tone
+#     - Eqnuiry: He wants more information, we need to respond in short and informative way, name dropping that we are the best, 
+#    respond in informative tone
 
 #     Please start the email with a warm introduction. Add the introduction if you need to.
     
@@ -30,6 +28,7 @@ from langchain.prompts import PromptTemplate
 from langchain.llms import OpenAI
 import os
 import openai
+from PIL import Image
 from dotenv import load_dotenv
 load_dotenv() 
 
@@ -68,16 +67,13 @@ prompt = PromptTemplate(
 )
 
 def load_LLM(openai_api_key):
-    """Logic for loading the chain you want to use should go here."""
     # Make sure your openai_api_key is set as an environment variable
     llm = OpenAI(temperature=.7, openai_api_key=openai.api_key)
     return llm
 
 st.set_page_config(page_title=" RockwoodGPT 📩 ", page_icon=":robot:")
 
-# Use HTML to center the title
-from PIL import Image
-
+#TITLE
 image = Image.open('logo.png')
 colA, colB = st.columns([1, 3])
 with colA:
@@ -85,18 +81,12 @@ with colA:
 with colB:
     st.header("  RockWood Email Generator ")
 
-# st.write("""
-#     <div style="display: flex; align-items: center; justify-content: center;">
-#         <img src="https://www.rockwoodglass.com/wp-content/uploads/2020/02/logo.png" alt="RWlogo" style="width: 50px; height: 50px;">
-#         <h2 style="margin-left: 10px;">📩 RockWood Email Generator</h2>
-#     </div>
-# """, unsafe_allow_html=True)
 st.write(" ")
 st.write(" ")
 st.write(" ")
 
 
-
+#LOGIC
 col1, col2 = st.columns([6, 12])
 with col1:
     sender = st.text_input(label="Name of the client *", key="sender_input")
@@ -137,6 +127,8 @@ with col4:
 
 
 st.write(" ")
+
+#MAIN
 if st.button("Generate REPLY 📩", type='secondary', help="Click to see an example of the email you will be creating."):
 
     if email_input and sender and recipient:
@@ -159,18 +151,3 @@ if st.button("Generate REPLY 📩", type='secondary', help="Click to see an exam
         st.write(formatted_email)
     else:
         st.warning('Please fill in all required fields (Name of the client, Your name, and paste the email).', icon="⚠️")
-
-# if st.button("Generate REPLY 📩", type='secondary', help="Click to see an example of the email you will be creating."):
-
-#     if email_input and sender and recipient:
-#         if not openai.api_key:
-#             st.warning('Please insert OpenAI API Key. Instructions [here](https://help.openai.com/en/articles/4936850-where-do-i-find-my-secret-api-key)', icon="⚠️")
-#             st.stop()
-
-#         llm = load_LLM(openai_api_key=openai.api_key)
-
-#         prompt_with_email = prompt.format(sender=sender, recipient=recipient, email=email_input)
-
-#         formatted_email = llm(prompt_with_email)
-
-#         st.write(formatted_email)
